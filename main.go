fed17ae29b40d8737105889ac02d5504129a2087
Maciek Chmielewski <maciej.chmielewski@simplicity-poland.pl>
2020-09-02 13:36:22 +0200

    init

---
+++ main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+type Article struct {
+	Title   string `json:"Title"`
+	Desc    string `json:"desc"`
+	Content string `json:"content"`
+}
+
+type Articles []Article
+
+func allArticles(w http.ResponseWriter, r *http.Request) {
+	articles := Articles{
+		Article{Title: "Test title", Desc: "Test Desc", Content: "Hello World"},
+	}
+	fmt.Println("Articles Endpoint")
+	json.NewEncoder(w).Encode(articles)
+}
+
+func addArticles(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Post articles")
+}
+
+func homePage(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Homepage")
+}
+
+func handleRequest() {
+	myRouter := mux.NewRouter().StrictSlash(true)
+
+	myRouter.HandleFunc("/", homePage)
+	myRouter.HandleFunc("/articles", allArticles).Methods("GET")
+	myRouter.HandleFunc("/articles", addArticles).Methods("POST")
+	log.Fatal(http.ListenAndServe(":80", myRouter))
+}
+
+func main() {
+	handleRequest()
+}
