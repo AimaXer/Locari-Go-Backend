f97b78fa999020db18b4660071aed85dbc08e0b1
Maciek Chmielewski <maciej.chmielewski@simplicity-poland.pl>
2020-09-07 16:04:43 +0200

    update

--- go.mod
+++ go.mod
@@ -2,4 +2,9 @@ module package
 
 go 1.15
 
-require github.com/gorilla/mux v1.8.0
+require (
+	github.com/GoogleCloudPlatform/cloudsql-proxy v1.17.0
+	github.com/gorilla/mux v1.8.0
+	github.com/jinzhu/gorm v1.9.16
+	github.com/lib/pq v1.3.0
+)
--- main.go
+++ main.go
@@ -1,78 +1,83 @@
 package main
 
 import (
-  "encoding/json"
-  "fmt"
-  "log"
-  "net/http"
+	"database/sql"
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
 
-  "github.com/gorilla/mux"
-  "github.com/jinzhu/gorm"
-  //"github.com/lib/pq"
-  //"github.com/rs/cors"
-  "github.com/GoogleCloudPlatform/cloudsql-proxy/proxy/dialers/postgres"
-  _ "github.com/jinzhu/gorm/dialects/postgres"
+	"github.com/gorilla/mux"
+	_ "github.com/lib/pq"
+
+	_ "github.com/jinzhu/gorm/dialects/postgres"
 )
 
-type Task struct {
-  gorm.Model
-  Title   string
-  Description    string
-  Content string
-}
-
-var db *gorm.DB
-var err error
-type Tasks []Task
+var (
+	db *sql.DB
+)
 
 func allTasks(w http.ResponseWriter, r *http.Request) {
-  var task Task
-  db.Find(&task)
-  json.NewEncoder(w).Encode(&task)
+	db, err := sql.Open("postgres", "host=34.77.175.38 user=aimaxer dbname=locari_db sslmode=disable password=Maciek0808")
+
+	if err != nil {
+
+		panic(err)
+
+	} else {
+		fmt.Printf("DB connected")
+	}
+
+	defer db.Close()
+	rows, _ := db.Query(fmt.Sprintf("SELECT * FROM tasks"))
+	for rows.Next() {
+		var (
+			title   string
+			desc    string
+			content string
+		)
+		if err := rows.Scan(&title, &desc, &content); err != nil {
+			log.Fatal(err)
+		}
+		fmt.Fprintf(w, "%s - %s - %s", title, desc, content)
+	}
+	json.NewEncoder(w).Encode(&rows)
 }
 
-func deleteTask(w http.ResponseWriter, r *http.Request){
-  fmt.Fprintf(w, "delete task")
+func deleteTask(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "delete task")
 }
 
 func addTasks(w http.ResponseWriter, r *http.Request) {
-  vars := mux.Vars(r)
-  taskAdd := Task{
-    Title : vars["title"],
-    Description : vars["description"],
-    Content : vars["content"],
-  }
-  
-  db.Create(&taskAdd)
+	db, err := sql.Open("postgres", "host=34.77.175.38 user=aimaxer dbname=locari_db sslmode=disable password=Maciek0808")
+
+	if err != nil {
+		panic(err)
+
+	} else {
+		fmt.Printf("DB connected\n")
+	}
+
+	defer db.Close()
+	db.Exec(fmt.Sprintf("INSERT INTO tasks (title, description, content) VALUES ('%s', '%s', '%s')", r.FormValue("title"), r.FormValue("description"), r.FormValue("content")))
+	fmt.Printf("Inserted \n")
 }
 
 func homePage(w http.ResponseWriter, r *http.Request) {
-  fmt.Fprintf(w, "Homepage-karolina")
+	fmt.Fprintf(w, "Homepage-karolina")
 }
 
 func handleRequest() {
-  myRouter := mux.NewRouter().StrictSlash(true)
-  
-  myRouter.HandleFunc("/", homePage)
-  myRouter.HandleFunc("/Tasks", allTasks).Methods("GET")
-  myRouter.HandleFunc("/Tasks", addTasks).Methods("POST")
-  myRouter.HandleFunc("/Tasks", deleteTask).Methods("DELETE")
-  log.Fatal(http.ListenAndServe(":8080", myRouter))
+	myRouter := mux.NewRouter().StrictSlash(true)
+
+	myRouter.HandleFunc("/", homePage)
+	myRouter.HandleFunc("/Tasks", allTasks).Methods("GET")
+	myRouter.HandleFunc("/Tasks", addTasks).Methods("POST")
+	myRouter.HandleFunc("/Tasks", deleteTask).Methods("DELETE")
+	log.Fatal(http.ListenAndServe(":8080", myRouter))
 }
 
 func main() {
-  
-  db, err := sql.Open( "cloudsqlpostgres", "host=34.77.175.38 user=postgres dbname=locari_db sslmode=disable password=Maciek0808")
 
-  if err != nil {
-
-    panic(err)
-
-  } else {
-    fmt.Printf("DB connected")
-  }
-  
-  defer db.Close()
-  
-  handleRequest()
+	handleRequest()
 }
