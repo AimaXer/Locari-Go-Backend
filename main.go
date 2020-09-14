5d6413c25d3f1b73e66ae177394e8475b8a11743
Maciek Chmielewski <maciej.chmielewski@simplicity-poland.pl>
2020-09-14 15:09:35 +0200

    u

--- main.go
+++ main.go
@@ -29,24 +29,32 @@ func handleRequest() {
 
 	myRouter.HandleFunc("/", homePage)
 	myRouter.HandleFunc("/Tasks", allTasks).Methods("GET")
-	myRouter.HandleFunc("/Tasks", addTasks).Methods("POST")
-	myRouter.HandleFunc("/Tasks", deleteTask).Methods("DELETE")
+	// myRouter.HandleFunc("/Tasks", addTasks).Methods("POST")
+	// myRouter.HandleFunc("/Tasks", deleteTask).Methods("DELETE")
 	log.Fatal(http.ListenAndServe(":8080", myRouter))
 }
 
 func allTasks(w http.ResponseWriter, r *http.Request) {
+	db, err := sql.Open("postgres", "host=34.77.175.38 user=aimaxer dbname=locari_db sslmode=disable password=Maciek0808")
+
+	if err != nil {
+		panic(err)
+	} else {
+		fmt.Printf("DB connected")
+	}
+
+	defer db.Close()
 
 	rows, _ := db.Query(fmt.Sprintf("SELECT * FROM tasks"))
 	for rows.Next() {
 		var (
 			title   string
-			desc    string
 			content string
 		)
-		if err := rows.Scan(&title, &desc, &content); err != nil {
+		if err := rows.Scan(&title, &content); err != nil {
 			log.Fatal(err)
 		}
-		fmt.Fprintf(w, "%s - %s - %s", title, desc, content)
+		// fmt.Fprintf(w, "%s - %s", title, content)
 	}
 	json.NewEncoder(w).Encode(&rows)
 }
